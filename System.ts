1520975e925bad58275bb6dbe700da71b5d344fb
guolei <31437616+958877748@users.noreply.github.com>
2025-09-04 17:51:53 +0800

    Update System.ts

--- System.ts
+++ System.ts
@@ -5,6 +5,7 @@ export default class System<W extends World = World> extends cc.Component {
     world: W
     ecsWorld: bitecs.World
     initializeSystem(world: W) {
+        this.world = world
     }
     onUpdate(dt: number) {
     }
